1091f9523bd5f3c67f43708701dd276c87230124
Kat Sauma <katsauma@gmail.com>
2021-02-12 14:42:29 -0800

    search.js state set, handlers, sort and order methods with search bar-search bar now .props instead of .state

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,14 +1,54 @@
 import React, { Component } from 'react'
 import PokeList from './PokeList.js';
 import pokemonImages from '../../Data.js';
+import SearchBar from './SearchBar.js';
 
 export default class Search extends Component {
+
+    state = {
+        pokemon: pokemonImages,
+        sortOrder: 'Ascend',
+        sortBy: 'pokemon',
+        filter: ''
+    }
+
+    handleSortOrder = (e) => {
+        this.setState({
+            sortOrder: e.target.value
+        })
+    }
+
+    handleSortBy = (e) => {
+        this.setState({
+            sortBy: e.target.value
+        })
+    }
+
+    handleFilter = (e) => {
+        this.setState({
+            filter: e.target.value
+        })
+    }
     render() {
+
+        if (this.state.sortBy !== '') {
+
+            if (this.state.sortOrder === 'Ascend') {
+                this.state.pokemon.sort((a, b) => a[this.state.sortBy].localeCompare(b[this.state.sortBy]))
+                //descending order
+            } else { this.state.pokemon.sort((a, b) => b[this.state.sortBy].localeCompare(a[this.state.sortBy])) };
+        }
+
+        const filteredPokemon = pokemonImages.filter(pokemon => pokemon.pokemon.includes(this.state.filter))
+        // JSX turning into HTML
         return (
             <div>
-                <aside className='searchBar'></aside>
-                <p>SEARCH PAGE</p>
-                <PokeList pokemonImages={pokemonImages} />
+                <aside className='searchBar'>
+                    <SearchBar currentValue={this.state.filter}
+                        handleChange={this.handleFilter} />
+                </aside>
+
+                <PokeList pokemonImages={filteredPokemon} />
             </div>
         )
     }
---
+++ src/Components/Search/SearchBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class SearchBar extends Component {
+    render() {
+        return (
+            <div>
+                <p>SEARCH PAGE</p>
+                <input value={this.props.currentValue}
+                    onChange={this.props.handleChange} />
+                <button>Submit</button>
+            </div>
+        )
+    }
+}
